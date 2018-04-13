f0ae4b1e0feca3cb77ddd42ba69955bb1934541a
Rushabh Patel <patelrushabh64@gmail.com>
2018-04-14 00:11:27 +0530

    Intergers, Floats and Arthematic Operators in Python

---
+++ WorkingWithNumbers.py
@@ -0,0 +1,27 @@
+num = 3
+
+numTwo = 3.014
+
+print(type(num))
+print(type(numTwo))
+
+#Arthematic Operators (basic)
+
+sum = num + numTwo;			print(sum)
+difference = num - numTwo;	print(difference)
+product = num * numTwo;		print(product)
+division = num / numTwo; 	print(division)     
+
+# complex
+
+#modulo
+
+mod = 3 % 2; 				print(mod)
+floorDiv = num // numTwo;	print(floorDiv)
+expo = 2 ** 10;				print(expo)
+
+
+
+
+
+
